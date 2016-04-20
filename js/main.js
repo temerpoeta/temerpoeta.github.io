82c843593ff64cff142c89fe847a44e1333b274a
gianordoli <gianordoligabriel@yahoo.com.br>
2016-04-20 02:17:45 -0400

    words

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ app.main = (function(){
 ['De vermelho', 'De preto', 'De verde', 'De amarelo', 'De rosa', 'De turquesa', 'De azul', 'De roxo', 'De lilás', 'De azul marinho', 'De cor de burro quando foge', 'De rosa bebê', 'De azul maravilha', 'De off-white'],
 ['Flamejante', 'Escaldante', 'Militante', 'Fumegante', 'Chamejante', 'Colorante', 'Exuberante', 'Deslumbrante', 'Estafante', 'Enfartante', 'Coadjuvante', 'Aconchegante', 'Gotejante', 'Rastejante'],
 ['Labaredas de fogo', 'Calabresas de molho', 'Milanesas ao forno', 'Cabeleiras de molho', 'Carambolas em jogo', 'Sabonete Sococo', 'Vai Botafogo', 'Eu to de fogo', 'Rodas de pogo', 'Vermelho agogô', 'Sdds do Togo', 'Ta ardendo o flogo', 'Recalcadas adogo ', 'Vestida de Gorpo'],
-['Olhos brilhantes', 'Boca irritante', 'Feios Mirantes', 'Cocôs de feirante', 'Testa de diamante', 'Suvacos de desodorante', 'Ombreiras exubeirantes', 'Fraldas e estimulantes', 'Umbigo retumbante', 'Orelhas de elefante', 'Costas escaldantes', 'Pés verossimilhantes', 'Perucas e corantes', 'Ganhos escoriantes'],
+['Olhos brilhantes', 'Boca irritante', 'Feios mirantes', 'Cocôs de feirante', 'Testa de diamante', 'Suvacos de desodorante', 'Ombreiras exubeirantes', 'Fraldas e estimulantes', 'Umbigo retumbante', 'Orelhas de elefante', 'Costas escaldantes', 'Pés verossimilhantes', 'Perucas e corantes', 'Ganhos escoriantes'],
 ['Que sorriem', 'Que riem', 'Carpe diem', 'Que auxiliem', 'Que inebriem', 'Que devaneiem', 'Que miem', 'Que piem', 'Que angariem', 'Que prestigiem', 'Que presidiem', 'Que estropiem', 'Que vaiem', 'Que odeiem'],
 ['Com lábios rubros', 'Com braços brutus', 'Com cílios burros', 'Com seios ombros', 'Com dentes sujos', 'Com tíbios xucros', 'Com pífios mucos', 'Com sábios lustrus', 'Com joelhos dúbios', 'Com prévios jorros', 'Com crio logros', 'Popeye e Brutus', 'Com bigodes lusos', 'Com caros juros'],
 ['Incêndios', 'Homicídios', 'Genocídios', 'Subsídios', 'Seios', 'Interlúdios', 'Latifúndios', 'Dispêndios', 'Selênios', 'Triênios', 'Betos e Ênios', 'Compêndios', 'Compêndio', 'Assédios'],
@@ -21,9 +21,9 @@ app.main = (function(){
 ['Incendiado', 'Pegando fogo', 'Fervendo', 'Chamuscado', 'Lascado', 'Ensebado', 'Arrasado', 'Desgraçado', 'Maquiado', 'Emcabaçado', 'Chatiado', 'Enviesado', 'Emparelhado', 'Enfiado'],
 ['Consumido', 'Feito', 'Devorado', 'Misturado', 'Engolido', 'Dividido', 'Resumido', 'Enxerido', 'Fedido', 'Tolhido', 'Fudido', 'Medido', 'Sofrido', 'Ressentido'],
 ['Dissolvido', 'Evaporado', 'Desfeito', 'Desligado', 'Dido-dido', 'Diluido', 'Traído', 'Vivido', 'Sabido', 'Polido', 'Resolvido', 'Bebido', 'Instruído', 'Cozido'],
-['Finalmente', 'Incondicionalmente', 'Gradativamente', 'Ultimamente', 'Afe-mente', 'Dualisticamente', 'Eternamente', 'Temente', 'Esporadicamente', 'Infelizmente', 'Inconstitucionalissimamente', 'Redondamente', 'Enfadonhamente', 'Bovinamente'],
-['Restam cinzas', 'Gastam as pingas', 'Com ojerizas', 'Restam lindas', 'E as ranzinzas', 'Restam balizas', 'Restam dindas', 'Difundidas', 'Sobram milhas', 'Somos ranzinzas', 'Bebi em Minas', 'Bazingas', 'Somos Maisas', 'Restam lendas'],
-['Que espalho na cama', 'Que jogo na lama', 'Que boto em cana', 'Que vejo a trama', 'Dividindo a cama', 'Molhando a cama', 'Que solta a escama', 'Embaixo da cama', 'Rolando na cama', 'Pulando na cama', 'Deserto atacama', 'Chama o Ibama', 'Montado na lhama', 'Que minto minha fama'],
+['Finalmente', 'Incondicionalmente', 'Gradativamente', 'Ultimamente', 'Recentemente', 'Dualisticamente', 'Eternamente', 'Temente', 'Esporadicamente', 'Infelizmente', 'Inconstitucionalissimamente', 'Redondamente', 'Enfadonhamente', 'Bovinamente'],
+['Restam cinzas', 'Gastam as pingas', 'Com ojerizas', 'Restam lindas', 'E as ranzinzas', 'Restam balizas', 'Restam dindas', 'Difundidas', 'Sobram milhas', 'Restam ranzinzas', 'Bebi em Minas', 'Bazingas', 'Somos Maisas', 'Restam lendas'],
+['Que espalho na cama', 'Que jogo na lama', 'Que boto em cana', 'Que vejo a trama', 'Que divido a cama', 'Que molho a cama', 'Que solto a escama', 'Embaixo da cama', 'Que rolo na cama', 'Pulando na cama', 'Deserto atacama', 'Chamo o Ibama', 'Que monto na lhama', 'Que minto minha fama'],
 ['Para dormir', 'Para redimir', 'Para eximir', 'Para possuir', 'Para mimir', 'Para cuspir', 'Para coibir', 'Para exprimir', 'Para reprimir', 'Para trair', 'Para Mi-Mir', 'Para resumir', 'Para oprimir', 'Para o Walmir'],
 	];
 
